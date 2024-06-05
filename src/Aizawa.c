9b32b680bc7cc85001221e42d3a2f4f502d21a09
0x1e9307 <wad1m.frwrd@gmail.com>
2024-06-05 03:39:05 +0300

    More settings

--- src/Aizawa.c
+++ src/Aizawa.c
@@ -1,6 +1,8 @@
 #include <GL/glut.h>
 #include <math.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 // Parameters of the Aizawa attractor
 double a = 0.95, b = 0.7, c = 0.6, d = 3.5, e = 0.25, f = 0.1;
@@ -10,7 +12,10 @@ double x = 0.1, y = 0, z = 0;
 double angleZ = 0.0;
 
 // Number of points of the attractor
-int numPoints = 40000;
+int numPoints = 30000;
+
+// Point size
+float pointSize = 1.0;
 
 // Full-screen mode switch
 int isFullScreen = 0;
@@ -21,6 +26,12 @@ float color[3] = {0.0, 1.0, 1.0};
 // Screen dimensions
 int screenWidth = 1920;
 int screenHeight = 1080;
+
+// Global variable to track point size and number of points
+char infoString[50];
+
+// Flag to control information display
+int showInfo = 1;
 
 // Function for numerical integration using the 4th order Runge-Kutta method
 void rk4_step(double *x, double *y, double *z, double dt) {
@@ -63,33 +74,58 @@ void rk4_step(double *x, double *y, double *z, double dt) {
     *z += (dt / 6.0) * (dz1 + 2 * dz2 + 2 * dz3 + dz4);
 }
 
-// Function to display the scene
-void display() {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear the color and depth buffers
-    glPushMatrix(); // Push the current matrix onto the matrix stack
-    glRotatef(90, 1.0, 0.0, 0.0); // Rotate by 90 degrees around the X axis
-    glRotatef(angleZ, 0.0, 0.0, 1.0); // Rotate around the Z axis based on the angleZ variable
-    glColor3fv(color); // Set the current color
-    glBegin(GL_POINTS); // Begin drawing points
-    for (int i = 0; i < numPoints; i++) {
-        rk4_step(&x, &y, &z, 0.1); // Calculate the next point of the attractor using the Runge-Kutta method
-        glVertex3f(x, y, z); // Specify the vertex coordinates of the point
+// Function to display information
+void renderBitmapString(float x, float y, void *font, const char *string) {
+    const char *c;
+    glRasterPos2f(x, y);
+    for (c = string; *c != '\0'; c++) {
+        glutBitmapCharacter(font, *c);
     }
-    glEnd(); // End drawing points
-    glPopMatrix(); // Pop the current matrix from the matrix stack
-    glutSwapBuffers(); // Swap the front and back buffers to display the rendered scene
 }
 
-// Function to set up the scene
-void setup() {
-    glClearColor(0.0, 0.0, 0.0, 1.0); // Set the clear color (black)
-    glPointSize(1.0); // Set the point size for rendering
-    glMatrixMode(GL_PROJECTION); // Set the current matrix mode to projection
-    glLoadIdentity(); // Load the identity matrix to reset the projection matrix
-    gluPerspective(24.0, (double) screenWidth / (double) screenHeight, 1.0, 100.0); // Set the perspective projection
-    glMatrixMode(GL_MODELVIEW); // Set the current matrix mode to modelview
-    glLoadIdentity(); // Load the identity matrix to reset the modelview matrix
-    glTranslatef(0.0, 0.0, -10.0); // Translate the scene along the z-axis to position it properly in the view
+// Function to display the scene
+void display() {
+    // Clear the color and depth buffers
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    // Push the current matrix onto the matrix stack
+    glPushMatrix();
+
+    // Rotate by 90 degrees around the X axis
+    glRotatef(90, 1.0, 0.0, 0.0);
+
+    // Rotate around the Z axis based on the angleZ variable
+    glRotatef(angleZ, 0.0, 0.0, 1.0);
+
+    // Set the current color
+    glColor3fv(color);
+
+    // Set the point size
+    glPointSize(pointSize);
+
+    // Begin drawing points
+    glBegin(GL_POINTS);
+
+    // Iterate through the points and draw them
+    for (int i = 0;  i < numPoints; i++) {
+        rk4_step(&x, &y, &z, 0.1);
+        glVertex3f(x, y, z);
+    }
+
+    // End drawing points
+    glEnd();
+
+    // Pop the current matrix from the matrix stack
+    glPopMatrix();
+
+    // Display the information if the flag is set
+    if (showInfo) {
+        glColor3f(1.0f, 1.0f, 1.0f);
+        renderBitmapString(-3.5f, 1.8f, GLUT_BITMAP_HELVETICA_12, infoString);
+    }
+
+    // Swap the front and back buffers to display the rendered scene
+    glutSwapBuffers();
 }
 
 // Function to handle animation
@@ -108,11 +144,14 @@ void keyboard(unsigned char key, int x, int y) {
                 glutReshapeWindow(960, 540); // Restore the window to its original size
                 isFullScreen = 0; // Toggle fullscreen mode off
             } else {
-                exit(0); // Exit the program
+                exit(0);
             }
             break;
         case 'f':
-            if (!isFullScreen) {
+            if (isFullScreen) {
+                glutReshapeWindow(960, 540); // Restore the window to its original size
+                isFullScreen = 0; // Toggle fullscreen mode off
+            } else {
                 glutFullScreen(); // Switch to fullscreen mode
                 isFullScreen = 1; // Toggle fullscreen mode on
             }
@@ -127,6 +166,9 @@ void keyboard(unsigned char key, int x, int y) {
             color[1] = 1.0;
             color[2] = 1.0;
             break;
+        case 'i': // Toggle information display
+            showInfo = !showInfo;
+            break;
     }
 }
 
@@ -134,13 +176,49 @@ void keyboard(unsigned char key, int x, int y) {
 void specialKeys(int key, int x, int y) {
     switch (key) {
         case GLUT_KEY_UP: // Increase the number of points
-            numPoints += 1000;
+            if (numPoints < 100000)
+                numPoints += 500;
             break;
         case GLUT_KEY_DOWN: // Decrease the number of points
-            if (numPoints > 1000) // To prevent the number of points from becoming less than 0
-                numPoints -= 1000;
+            if (numPoints > 500) // To prevent the number of points from becoming less than 0
+                numPoints -= 500;
+            break;
+        case GLUT_KEY_RIGHT: // Increase point size
+            if (pointSize < 3.0)
+                pointSize += 0.1;
+            break;
+        case GLUT_KEY_LEFT: // Decrease point size
+            if (pointSize > 0.2)
+                pointSize -= 0.1;
             break;
     }
+    sprintf(infoString, "Points: %d, Size: %.1f", numPoints, pointSize);
+}
+
+// Function to set up the scene
+void setup() {
+    // Set the clear color (black)
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+    // Set the point size for rendering
+    glPointSize(pointSize);
+
+    // Set the current matrix mode to projection
+    glMatrixMode(GL_PROJECTION);
+
+    // Load the identity matrix to reset the projection matrix
+    glLoadIdentity();
+
+    // Set the perspective projection
+    gluPerspective(24.0, (double) screenWidth / (double) screenHeight, 1.0, 100.0);
+
+    // Set the current matrix mode to modelview
+    glMatrixMode(GL_MODELVIEW);
+
+    // Load the identity matrix to reset the modelview matrix
+    glLoadIdentity();
+
+    // Translate the scene along the z-axis to position it properly in the view
+    glTranslatef(0.0, 0.0, -10.0);
 }
 
 int main(int argc, char** argv) {
@@ -149,14 +227,16 @@ int main(int argc, char** argv) {
     glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH); // Set display mode
     glutInitWindowSize(960, 540); // Set initial window size
     glutCreateWindow("Aizawa Attractor"); // Create a window with the given title
-
     glEnable(GL_DEPTH_TEST); // Enable depth testing for 3D rendering
 
     // Register callback functions for various events
     glutDisplayFunc(display); // Function to render the scene
+    sprintf(infoString, "Points: %d, Size: %.1f", numPoints, pointSize);
     glutIdleFunc(idle); // Function to handle idle state
     glutKeyboardFunc(keyboard); // Function to handle keyboard input
     glutSpecialFunc(specialKeys); // Function to handle special keys
+    
+    glutPostRedisplay(); // Request a redraw of the scene
 
     setup(); // Initialize the scene
 
